a2f72982e78ba32f0e68af78403466ba32c5d4ab
solee0524 <iblee0524@gmail.com>
2018-03-22 15:21:24 +0800

    Add core code

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/**
+ * Created by solee on 2018/3/22.
+ */
+
+'use strict';
+
+module.exports = async function (options) {
+  var data = options.data || [];
+  var fields = options.fields || [];
+  var delimiter = options.delimiter || ',';
+
+  if (fields.length === 0) {
+    return 'Need Fields!';
+  }
+
+  if (!Array.isArray(data) || !Array.isArray(fields)) {
+    return 'Data and Fields should be array!';
+  }
+
+  var csv = '';
+  csv += fields.join(delimiter) + '\n';
+
+  for (let item of data) {
+    var tmpRow = [];
+    for (let field of fields) {
+      var tmpField = field.split('.');
+      var tmpValue = '';
+      if (tmpField.length === 1) {
+        tmpValue = item[field] === undefined || item[field] === null
+          ? ''
+          : item[field];
+      } else {
+        tmpValue = item;
+        var n = 0;
+        while (n < tmpField.length) {
+          tmpValue = tmpValue[tmpField[n]] === undefined || tmpValue[tmpField[n]] === null
+            ? ''
+            : tmpValue[tmpField[n]];
+          if (!tmpValue) {
+            break;
+          }
+          n++;
+        }
+      }
+
+      if (Array.isArray(tmpValue)) {
+        tmpValue = '"' + String(tmpValue).replace(/"/g, '""') + '"';
+      } else {
+        tmpValue = String(tmpValue).replace(/"/g, '""');
+      }
+
+      tmpRow.push(tmpValue);
+    }
+    csv += tmpRow.join(delimiter) + '\n';
+  }
+
+  return csv;
+};
